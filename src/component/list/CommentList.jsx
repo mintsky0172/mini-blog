c325f76100b3b25daef989e88a4f822e398714c0
mintsky0172 <mintsky0172@naver.com>
2024-11-25 23:17:36 +0900

    수정

--- src/component/list/CommentList.jsx
+++ src/component/list/CommentList.jsx
@@ -3,7 +3,10 @@ import styled from "styled-components";
 import CommentListItem from "./CommentListItem"; // Assuming CommentListItem is correctly exported
 
 const Wrapper = styled.div`
-  // ...
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
 `;
 
 function CommentList(props) {
